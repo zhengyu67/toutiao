672f43de1b994b6e820dbc572c9f3086df9fe96d
zhengyu <1130367447@qq.com>
2021-03-24 15:40:02 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import Vant from 'vant';
+import 'vant/lib/index.css';
 // 引入全局样式
 import './styles/index.less'
 Vue.config.productionTip = false
-
+Vue.use(Vant);
 new Vue({
   router,
   store,
